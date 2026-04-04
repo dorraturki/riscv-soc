df205b2b085f6e8d7ba218213ed4c28bea68fec5
Dorra Turki <dorraturki123@gmail.com>
2026-04-04 11:04:48 +0100

    Enhance tb_ALU with reference ALU for validation
    Added a reference ALU for comparison in the test bench.

--- vhdl/test_benchs/tb_ALU.vhd
+++ vhdl/test_benchs/tb_ALU.vhd
@@ -6,34 +6,35 @@ library RISCV;
 use RISCV.pack_RISCV32I_types.all;
 use RISCV.pack_RISCV32I_components.all;
 
+library RISCV_reference;
+
 entity tb_ALU is
 end entity;
 
 architecture a of tb_ALU is
 
    for DUT           : ALU use entity riscv.ALU(a);
+   for DUT_reference : ALU use entity riscv_reference.ALU(a);
 
    signal op : toperation;
-   signal a  : std_logic_vector(31 downto 0);
-   signal b  : std_logic_vector(31 downto 0);
-   signal c  : std_logic_vector(31 downto 0);
+   signal a  : std_logic_vector(32-1 downto 0);
+   signal b  : std_logic_vector(32-1 downto 0);
+   signal c  : std_logic_vector(32-1 downto 0);
 
-   signal c_reference : std_logic_vector(31 downto 0);
+   signal c_reference : std_logic_vector(32-1 downto 0);
    signal c_ok        : boolean;
-
 
 begin
 
-   -- Device Under Test
    DUT : ALU
-      port map(
-         op => op,
-         a  => a,
-         b  => b,
-         c  => c
-      );
+	port map(
+      op => op,
+      a  => a,
+      b  => b,
+      c  => c 
+	);
 
-   stim: process
+   process
    begin
       -- ADD / SUB
       op <= OP_ADD;   a <= std_logic_vector(to_signed(-2,32)); b <= std_logic_vector(to_signed( 5,32)); wait for 100 ns;
@@ -68,4 +69,15 @@ begin
 
    end process;
 
-end architecture;
+   DUT_reference : ALU
+	port map(
+      op => op,
+      a  => a,
+      b  => b,
+      c  => c_reference 
+	);
+
+   --Assertions de validation
+   c_ok <= true when c=c_reference else false;
+
+end a;
