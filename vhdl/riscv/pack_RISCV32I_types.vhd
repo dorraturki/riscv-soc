bc03a735e2f4d175c8b047b0ac6d8b7c0dcf807f
Dorra Turki <dorraturki123@gmail.com>
2026-04-06 11:13:51 +0100

    Refactor operation types and add RV32I constants
    Reorganized operation types and added RV32I opcodes and function fields.

--- vhdl/riscv/pack_RISCV32I_types.vhd
+++ vhdl/riscv/pack_RISCV32I_types.vhd
@@ -3,29 +3,32 @@ use ieee.std_logic_1164.all;
 
 package pack_RISCV32I_types is
                         
-   type toperation is(
-      --Arithmétique entiers
-      OP_ADD  ,   --Addition signée
-      OP_SUB  ,   --Soustraction signée
-      --Fonctions logiques
-      OP_AND  ,   --ET
-      OP_OR   ,   --OU
-      OP_XOR  ,   --OU exclusif
-      --Tests conditionnels
-      OP_EQ   ,   --Egalité
-      OP_NEQ  ,   --Différence
-      OP_LT   ,   --Inférieur ou égal signé
-      OP_LTU  ,   --Inférieur ou égal non signé
-      OP_GE   ,   --Supérieur ou égal signé
-      OP_GEU  ,   --Supérieur ou egal non signé
-      --Décalages
-      OP_SLL  ,   --Décalage logique à gauche
-      OP_SRL  ,   --Décalage logique à droite
-      OP_SRA  ,   --Décalage arithmétique à droite
-      --Autres
-      OP_PASSA,   --Copie A en sortie   
-      OP_PASSB    --Copie B en sottie
+   type toperation is (
+      OP_ADD  , OP_SUB ,
+      OP_AND  , OP_OR  , OP_XOR,
+      OP_EQ   , OP_NEQ,
+      OP_LT   , OP_LTU,
+      OP_GE   , OP_GEU,
+      OP_SLL  , OP_SRL , OP_SRA,
+      OP_PASSA, OP_PASSB
    );
-      
+
+   -- RV32I Opcodes
+   constant OPCODE_LOAD      : std_logic_vector(7-1 downto 0) := b"00_000_11";
+
+   constant OPCODE_STORE     : std_logic_vector(7-1 downto 0) := b"01_000_11";
+
+   -- RV32I Function 3 field
+   constant F3_LB    : std_logic_vector(3-1 downto 0) := b"000";
+   constant F3_LH    : std_logic_vector(3-1 downto 0) := b"001";
+   constant F3_LW    : std_logic_vector(3-1 downto 0) := b"010";
+   constant F3_LBU   : std_logic_vector(3-1 downto 0) := b"100";
+   constant F3_LHU   : std_logic_vector(3-1 downto 0) := b"101";
+
+   constant F3_SB    : std_logic_vector(3-1 downto 0) := b"000";
+   constant F3_SH    : std_logic_vector(3-1 downto 0) := b"001";
+   constant F3_SW    : std_logic_vector(3-1 downto 0) := b"010";
+
 end package;
+
 
