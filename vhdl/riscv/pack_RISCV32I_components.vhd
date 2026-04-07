4f761e1955a736ce8d54138336ab56662d1dd672
Dorra Turki <dorraturki123@gmail.com>
2026-04-07 09:40:56 +0100

    Add new components to pack_RISCV32I_components package

--- vhdl/riscv/pack_RISCV32I_components.vhd
+++ vhdl/riscv/pack_RISCV32I_components.vhd
@@ -41,6 +41,7 @@ package pack_RISCV32I_components is
       );
    end component;
 
+   component input_data_manager
       port(
          inst     : in  std_logic_vector(32-1 downto 0);
          BE       : in  std_logic_vector( 4-1 downto 0);
@@ -54,6 +55,37 @@ package pack_RISCV32I_components is
          inst     : in  std_logic_vector(32-1 downto 0);
          data_in  : in  std_logic_vector(32-1 downto 0);
          data_out : out std_logic_vector(32-1 downto 0)
+      );
+   end component;
+
+   component decoder
+      port(
+         inst : in  std_logic_vector(32-1 downto 0);
+         rs1a : out std_logic_vector( 5-1 downto 0);
+         rs2a : out std_logic_vector( 5-1 downto 0);
+         rda  : out std_logic_vector( 5-1 downto 0);
+         immI : out std_logic_vector(32-1 downto 0);
+         immS : out std_logic_vector(32-1 downto 0);
+         immB : out std_logic_vector(32-1 downto 0);
+         immU : out std_logic_vector(32-1 downto 0);
+         immJ : out std_logic_vector(32-1 downto 0)
+      );
+   end component;
+
+   component controller
+      port(
+         inst          : in  std_logic_vector(32-1 downto 0);
+         C             : in  std_logic_vector(32-1 downto 0);
+         decoded_inst  : out tinstruction;
+         reg_write     : out std_logic;
+         PCrs1d_select : out integer range 0 to 1;
+         imm4BJI_select: out integer range 0 to 3;
+         A_select      : out integer range 0 to 1;
+         B_select      : out integer range 0 to 4;
+         op            : out toperation;
+         data_write    : out std_logic;
+         data_read     : out std_logic;
+         rdd_select    : out integer range 0 to 1
       );
    end component;
 
